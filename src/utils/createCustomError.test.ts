05408dca1a719c2f2a51d3242d4d4a7fdb0ac19e
David Arques <david.darqsa@gmail.com>
2022-08-29 18:50:45 +0200

    Tested custom error function

---
+++ src/utils/createCustomError.test.ts
@@ -0,0 +1,20 @@
+import { CustomError } from "../server/types/errorInterfaces";
+import createCustomError from "./createCustomError";
+
+describe("Given a createCustomError function", () => {
+  describe("When invoked and gets 222 as code, 'hola' as message and the error message as private", () => {
+    test("Then it should return a new error with code 222, public message and private message", () => {
+      const code = 222;
+      const publicMessage = "hola";
+
+      const fakeError = createCustomError(code, publicMessage, Error.name);
+
+      const expectedError = new Error() as CustomError;
+      expectedError.statusCode = code;
+      expectedError.publicMessage = publicMessage;
+      expectedError.privateMessage = Error.name;
+
+      expect(fakeError).toStrictEqual(expectedError);
+    });
+  });
+});
