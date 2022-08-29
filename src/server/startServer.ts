622d0b59b510dcdfdea560f56871fac9cb599380
David Arques <david.darqsa@gmail.com>
2022-08-29 16:39:05 +0200

    Added startServer function and installed morgan+cors

---
+++ src/server/startServer.ts
@@ -0,0 +1,20 @@
+import chalk from "chalk";
+import Debug from "debug";
+import app from ".";
+
+const debug = Debug("users:server:startServer");
+
+const startServer = (port: number) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      debug(chalk.blue(`Server listening on http://localhost:${port}`));
+      resolve(true);
+    });
+
+    server.on("error", (error) => {
+      debug(`Error conecting to database: ${error}`);
+      reject(error);
+    });
+  });
+
+export default startServer;
