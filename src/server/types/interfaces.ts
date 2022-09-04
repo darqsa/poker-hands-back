f9eee495f093a1ffb82dd970c8edaa221a5a72f6
David Arques <david.darqsa@gmail.com>
2022-09-04 18:34:04 +0200

    Added hand interface

--- src/server/types/interfaces.ts
+++ src/server/types/interfaces.ts
@@ -23,3 +23,48 @@ export interface UserPayload {
 export interface CustomRequest extends Request {
   payload: UserPayload;
 }
+
+export interface HandData {
+  handName: string;
+  preGame: {
+    hero: {
+      position: number;
+      initialStack: number;
+      hand: string[];
+    };
+    villains: [
+      {
+        position: number;
+        initialStack: number;
+        hand: string[];
+      }
+    ];
+  };
+  game: {
+    preFlop: {
+      pot: number;
+      actions: string[];
+    };
+    flop?: {
+      board: string[];
+      pot: number;
+      actions: string[];
+    };
+    turn?: {
+      board: string[];
+      pot: number;
+      actions: string[];
+    };
+    river?: {
+      board: string[];
+      pot: number;
+      actions: string[];
+    };
+  };
+  postGame: {
+    finalPot: number;
+    gameWinner: string;
+    handDescription?: string;
+    handImage?: string;
+  };
+}
